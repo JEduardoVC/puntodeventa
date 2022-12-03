ec1ba1334dfab3a816a791c9942c87702ad5bc1e
José Eduardo Valencia Camacho <l18141024@queretaro.tecnm.mx>
2022-12-02 23:08:19 -0600

    Productos

--- app/Http/Requests/Category/StoreRequest.php
+++ app/Http/Requests/Category/StoreRequest.php
@@ -4,23 +4,10 @@ namespace App\Http\Requests\Category;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
+class StoreRequest extends FormRequest{
+    public function authorize() {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
---
+++ app/Http/Requests/Product/UpdateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function authorize() {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name'=>'required|string|max:50|unique:products,'. $this->route("product")->id,
+            'image'=>'required|dimensions:min_width=100,min_height=200',
+            'sell_price'=>'required',
+            'category_id'=>'required|integer|exists:App\Category,id',
+            'provider_id'=>'required|integer|exists:App\Provider,id',
+        ];
+    }
+
+    public function messages(){
+        return[
+            'name.required'=>'Este campo es requerido',
+            'name.string'=>'El valor no es correcto',
+            'name.max'=>'Solo se peremite 50 caracteres',
+            'name.unique'=>'Ya se tiene registrado',
+
+            'image.required'=>'Este campo es requerido',
+            'image.dimensions'=>'Solo se permiten imagenes de 100x200px',
+
+            'sell_price.required'=>'Este campo es requerido',
+
+            'category_id.required'=>'Este campo es requerido',
+            'category_id.integer'=>'El valor tiene que ser entero',
+            'category_id.exists'=>'La categoria no existe',
+
+            'provider_id.required'=>'Este campo es requerido',
+            'provider_id.integer'=>'El valor tiene que ser entero',
+            'provider_id.exists'=>'La categoria no existe',
+        ];
+    }
+}

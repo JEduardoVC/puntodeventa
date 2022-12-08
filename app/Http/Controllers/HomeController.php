ef3e78a526e1dce94f9ca6ca57fd92ce42116664
Luis Gerardo Valencia Camacho <70810750+LuisGerardoValenciaCamacho@users.noreply.github.com>
2022-12-08 12:40:27 -0600

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,11 +13,11 @@ class HomeController extends Controller
     }
 
     public function index() {
-        $comprasMes=DB::select("SELECT monthname(c.purchase_date) as mes, sum(c.total) as totalmes from purchases c where c.status='VALID' group by monthname(c.purchase_date) order by monthname(c.purchase_date) desc limit 12;");
+        $comprasMes=DB::select("SELECT monthname(c.purchase_date) as mes, ifnull(sum(c.total),0) as totalmes from purchases c where c.status='VALID' group by monthname(c.purchase_date) order by monthname(c.purchase_date) desc limit 12;");
 
-        $ventasMes=DB::select("SELECT monthname(v.sale_date) as mes, sum(v.total) as totalmes, month(v.sale_date) as mes_numero, year(v.sale_date) from sales v where v.status='VALID' group by monthname(v.sale_date),month(v.sale_date), year(v.sale_date) order by year(v.sale_date), month(v.sale_date) asc limit 12");
+        $ventasMes=DB::select("SELECT monthname(v.sale_date) as mes, ifnull(sum(v.total),0) as totalmes, month(v.sale_date) as mes_numero, year(v.sale_date) from sales v where v.status='VALID' group by monthname(v.sale_date),month(v.sale_date), year(v.sale_date) order by year(v.sale_date), month(v.sale_date) asc limit 12;");
 
-        $ventasDia=DB::select("SELECT DATE_FORMAT(v.sale_date,'%d/%m/%Y') as dia, sum(v.total) as totaldia from sales v where v.status='VALID' group by DATE_FORMAT(v.sale_date,'%d/%m/%Y') desc limit 12");
+        $ventasDia=DB::select("SELECT DATE_FORMAT(v.sale_date,'%d/%m/%Y') as dia, ifnull(sum(v.total),0) as totaldia from sales v where v.status='VALID' group by DATE_FORMAT(v.sale_date,'%d/%m/%Y');");
 
         $totales=DB::select("SELECT (select ifnull(sum(c.total),0) from purchases c where DATE(c.purchase_date) and c.status='VALID') as totalcompra,(select ifnull(sum(v.total),0) from sales v where DATE(v.sale_date) and v.status='VALID') as totalventa;");
 
